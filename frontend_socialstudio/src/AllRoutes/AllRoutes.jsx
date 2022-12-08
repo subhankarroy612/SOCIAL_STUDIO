d3d5922b81ac42308a41e81745756b68519cb3e8
subhankarroy612 <subhankarroy612@gmail.com>
2022-12-08 11:20:36 +0530

    progress

--- frontend_socialstudio/src/AllRoutes/AllRoutes.jsx
+++ frontend_socialstudio/src/AllRoutes/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
+import PrivateRoute from '../hoc/PrivateRoute';
 import Home from '../Pages/Home';
 import Login from '../Pages/Login'
 import Register from '../Pages/Register';
@@ -7,7 +8,7 @@ import Register from '../Pages/Register';
 
 const AllRoutes = () => {
     return <Routes>
-        <Route path='/' element={<Home />} ></Route>
+        <Route path='/' element={<PrivateRoute> <Home /> </PrivateRoute>} ></Route>
         <Route path='/login' element={<Login />} ></Route>
         <Route path='/register' element={<Register />} ></Route>
     </Routes>
---
+++ frontend_socialstudio/src/hoc/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+
+export default function PrivateRoute({children}) {
+
+  const { isAuth } = useSelector(store=>store.auth)
+
+  if(!isAuth){
+    return <Navigate to='/login' />
+  }
+  
+  return children
+}
